5eed9a2658c2c9881cd5cdaa82b5a7ed23d84e67
Evan <evanacoates@gmail.com>
2023-04-23 14:48:54 -0400

    index and notes get completed

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) =>
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/index.html"))
+});
 
-app.get('/notes', (req, res) =>
+
+app.get('/notes', (req, res) =>{
+    res.sendFile(path.join(__dirname, "/notes.html"))
+});
