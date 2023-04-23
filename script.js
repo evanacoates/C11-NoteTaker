800b41d04522e26c427fb9b867c75f19588859c8
Evan <evanacoates@gmail.com>
2023-04-23 14:53:42 -0400

    reformat

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
+
 const PORT = process.env.PORT || 3001;
 
 const app = express();
@@ -10,7 +11,6 @@ app.use(express.json());
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, "/index.html"))
 });
-
 
 app.get('/notes', (req, res) =>{
     res.sendFile(path.join(__dirname, "/notes.html"))
